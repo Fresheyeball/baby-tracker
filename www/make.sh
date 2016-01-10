e8cce9c9122e39d00c4bdf3e6c3e7f3fe1a5321a
Isaac Shapira <fresheyeball@gmail.com>
2016-01-10 03:09:07 -0700

    better syntax

--- www/make.sh
+++ www/make.sh
@@ -1,2 +1,4 @@
-for f in src/Native/*.wisp ; do cat "$f" | wisp --no-map > "${f%.wisp}.js" ; done
+for f in src/Native/*.wisp
+  do cat "$f" | wisp --no-map > "${f%.wisp}.js"
+done
 elm make src/Main.elm --output=js/index.js
